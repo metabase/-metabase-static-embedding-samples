c5995a11fc68bb3bb9cebc4d63ac2699773120e0
Alberto Perdomo <alberto.perdomo@gmail.com>
2024-11-19 18:54:24 +0000

    Include html and body tags

--- python/app.py
+++ python/app.py
@@ -15,12 +15,17 @@ METABASE_EMBED_DASHBOARD_ID = int(os.environ.get('METABASE_EMBED_DASHBOARD_ID', 
 def static_embed():
     payload = {
         "resource": {"dashboard": METABASE_EMBED_DASHBOARD_ID},
-        "params": {        
+        "params": {
         },
         "exp": round(time.time()) + (60 * 10) # 10 minute expiration
     }
     token = jwt.encode(payload, METABASE_EMBEDDING_SECRET, algorithm="HS256")
     iframeUrl = urljoin(METABASE_SITE_URL, "/embed/dashboard/" + token + "#bordered=true&titled=true")
     return f'''<script src="{METABASE_SITE_URL}/app/iframeResizer.js"></script>
-            <iframe src="{iframeUrl}" frameborder="0" width="800" height="600" onload="iFrameResize({{}}, this)" allowtransparency></iframe>
+            <!DOCTYPE html>
+<html>
+<body>
+    <iframe src="{iframeUrl}" frameborder="0" width="1800" height="3000" onload="iFrameResize({{}}, this)" allowtransparency></iframe>
+</body>
+</html>
             '''
